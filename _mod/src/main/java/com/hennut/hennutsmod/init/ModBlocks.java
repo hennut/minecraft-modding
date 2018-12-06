4589cc72e65efe931c1958f38aa948792caa56c8
hennut <lrhemmerling@gmail.com>
2018-12-06 19:10:57 +0100

    recipes and variants

--- _mod/src/main/java/com/hennut/hennutsmod/init/ModBlocks.java
+++ _mod/src/main/java/com/hennut/hennutsmod/init/ModBlocks.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.hennut.hennutsmod.blocks.BlockBase;
+import com.hennut.hennutsmod.blocks.pot.ClayPot;
+import com.hennut.hennutsmod.blocks.pot.IronPot;
 import com.hennut.hennutsmod.blocks.pot.WoodPot;
 
 import net.minecraft.block.Block;
@@ -16,7 +18,12 @@ public class ModBlocks {
 	public static final Block TEST_BLOCK = new BlockBase("testblock", Material.CLAY);
 	
 	//Pots
-	public static final Block WOOD_POT = new WoodPot("woodpot");
-	public static final Block CLAY_POT = new WoodPot("claypot");
-	public static final Block IRON_POT = new WoodPot("ironpot");
+	public static final Block OAK_WOOD_POT = new WoodPot("oakwoodpot");
+	public static final Block BIRCH_WOOD_POT = new WoodPot("birchwoodpot");
+	public static final Block SPRUCE_WOOD_POT = new WoodPot("sprucewoodpot");
+	public static final Block ACACIA_WOOD_POT = new WoodPot("acaciawoodpot");
+	public static final Block BIG_OAK_WOOD_POT = new WoodPot("bigoakwoodpot");
+	public static final Block JUNGLE_WOOD_POT = new WoodPot("junglewoodpot");
+	public static final Block CLAY_POT = new ClayPot("claypot");
+	public static final Block IRON_POT = new IronPot("ironpot");
 }
